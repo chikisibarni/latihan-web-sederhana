df9fead62a816c82382075e2ae68637eef4d0702
chikisibarni <chikiputri57@gmail.com>
2024-07-02 22:06:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+echo "Saya Chiki Dwi Putri Sibarani, dengan NIM 2215101013, Kelas A Reguler, Semester 4, sedang belajar membuat web sederhana yaitu menggunakan docker.
+Guna memenuhi tugas dari matakuliah Sistem Terdistribusi. Disini saya akan menceritakan sedikit tentang saya, diawali dari nama saya, diamana namaya 
+diartikan yaitu Dua Putri Cantik, sebenarya saya tdk paham dengan maksud nama saya, mengapa saya katakan seperti itu? karena saya memiliki 3 lagi
+saudara perempuan, apakah dari 4 anak perempuan ibu ku hanya dua orang kah yang cantik terus yang lainya disebut apa? hahaha lucunya itu la keluarga kami.
+Disini saya akan bercerita tentang masa perjalan saya sebagai anak rantau yang bener-bener jauh dari keluarga. Dimana pada saat itu saya sudah menduduki
+bangku kls 2 SMK, dan saya bingung apakah saya langsung kerja saja dan jika sudah punya cukup uang saya akan melamar menjadi TNI atau mengabulkan 
+permintaa orang tau atau pun keluarga saya. Saya bener-benar binggung, sehingga suatu saat saya dipanggil oleh Bapak Kepala Sekolah saya, bapak 
+memberikan sedikit pencerahan kepada saya betapa pentingnya akan pendidikan itu. Namun pada saat itu saya masih memikirkan jika saya kerja dan menghasilkan
+uang yang begitu banyka orang tua dan keluarga ku pasti senang, namun ternyata kebalikanya. Orang tua dan saudaraku berkata, hanya saya lagi harapan
+orang tua saya, dimana untuk mencapai keiinginan orang tua saya, disitu saya tetap bingung, dan bertanya di hatiku 'kenapa harus saya' sedangkan 
+saya masih memiliki 2 orang adik lagi, namun seiring berjalannya waktu saya sadar mengapa saya disuruh untuk mencapai keinginan orang tua saya karena
+keluarga ku melihat bahwa saya tidak pernah main-main dalam melakukan sesuatu. Walaupun demikian tiba la pada saatnya beberapa hari lagi pendaftaran
+'SNMPT' dimana itu adalah sebuah portal untuk siswa yang berprestasi masuk keperguruan tinggi, dan disitu pun saya masih tetap bilang ke guru-guru saya 
+bahwa saya tdk akan lanjut 'Kuliah' karena saya tdk mau membebani orang tua saya. Dan tak diduga sehari sebelum pendaftaraan 'SNMPT' tiba ibuku sdh 
+datang ke kantor kepsek bahwa ia membarikan surat persetujuan supaya saya daftar, beberapa setelah itu ibu mungkin sadar bahwa jika saya pergi tdk akan
+ada lagi yang akan mengerjakan pekerjaan rumah dan selalu bersama mereka. Beberapa hari sebelum pengumuman KIP-K ibu tdk menyapa aku bahkan tdk mau memakan
+masakkanku, saya berpiki apakah saya sdh berbuat salah pada ibu, namun ayah ku menghampiri diriku dan mengatakan bahwa aku tdk salah, hanya saja ibu ku
+merasa sedih karena akan jauh dari ku, hingga la penggumuman 'KIP-K' disitu saya sedang membantu ibu saya berjualan dan kami nangis berdua di pasar 
+itu karena saya di nyatkan lulus KIP-K, saya sangat mengucap syukur ke pada TUHAN karena dia membantu untuk mengurangi beban orang tuaku. singkat cerita 
+tibala saatnya saya pergi ke Bali dimana tempat saya menuntu ilmu, setiap hari saya menelpon ibu dan ayah saya, seiring berjalannya waktu, dimana pada 
+tahun 2023 tepat di bulan kelahiran saya dan ibu saya di tengah-tengah tanggal kelahiran kami ayah saya Meninggal Dunia, dimana disiang hari saya masih
+berbincang dengan ibu saya. dan tepat pada sorenya tenyata ayah ku sudah menghembuskan nahaf terakhirnya. Disitu saya sanggat menyesal karena ayah dan 
+ibu ku dulu berkata kepadaku bawasanya ' sayalah yang akan mengurus mereka kelak, namun disaat ayah saya menghembuskan nafas terakhir saya tdk ada.
+Namun saya berusaha tabah dan tetap kuat untuk mencapai keinginan orang tua saya, walaupun terkada merasakan sakit, depresi, senang, capek dll, Saya percaya
+bahwa TUHAN tdk akan membiarkan hambanya dalam kesulitan, apapun yang terjadi dikeluarga saya, saya percaya bahwa itu atas kehendak TUHAN, TUHAN tau apa
+yang terbaik untuk anak-anaknya. Sekian ceita saya Terimkasihhhhh ";
